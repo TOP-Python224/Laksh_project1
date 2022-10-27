473f1a3fb1075ba9ac80e8717fc24f2ea5983c56
Gennadiy <renardes@gmail.com>
2022-10-27 18:32:52 +0500

    load_ini(), write_ini(), draw_board() и тестовые данные .ini файлов

--- functions.py
+++ functions.py
@@ -15,15 +15,17 @@ def load_ini() -> bool:
     players.read(players_file, encoding='utf-8')
     for section in players.sections():
         STATS[section] = {
-            key: int(value)
+            # ИСПРАВИТЬ: не все значения являются числами
+            key: value
             for key, value in players[section].items()
         }
     saves = ConfigParser()
     saves.read(saves_file, encoding='utf-8')
     for section in saves.sections():
-        player = tuple(section.split(';'))
+        players = tuple(section.split(';'))
+        # ИСПРАВИТЬ: а здесь все значения как раз являются числами и необходимо преобразование
         turn = [i for i in saves[section]['turns'].split(',')]
-        SAVES[player] = turn
+        SAVES[players] = turn
     if STATS:
         return False
     else:
@@ -40,6 +42,7 @@ def write_ini() -> None:
     for save in SAVES:
         player_a, player_b = list(save)
         section = f"{player_a};{player_b}"
+        # ИСПРАВИТЬ: метод join() принимает на вход последовательности, содержащие только строки, поэтому необходимо обратное преобразование в строку для каждого элемента
         saves[section]['turns'] = ','.join([i for i in SAVES[save]])
     with open(saves_file, 'w', encoding='utf-8') as ini_file:
         saves.write(ini_file)
@@ -48,13 +51,15 @@ def write_ini() -> None:
 def draw_board(align_right: bool = False) -> str:
     """Формирует и возвращает строку, содержащую псевдографическое изображение игрового поля со сделанными ходами."""
     max_width = len(max(BOARD, key=lambda elem: len(elem)))
-    h_line = '—' * (DIM * (max_width + 2)) + '\n'
+    # КОММЕНТАРИЙ: при такой длине горизонтального разделителя у крайних ячеек не будет внешнего отступа от предполагаемого края
+    h_line = '—'*(DIM*(max_width + 2)) + '\n'
     result = ''
     if align_right:
         align = get_terminal_size()[0] - 2
     else:
         align = len(h_line)
     for i in RANGE:
+        # КОММЕНТАРИЙ: максимальная ширина ячейки вычисляется для того, чтобы ячейки нашего поля были одинаковой ширины и могли вместить не только односимвольные значения (см. тест ниже)
         row = ' | '.join(BOARD[i*DIM:(i+1)*DIM])
         result += f"{row.rjust(align)}\n"
         if i != DIM - 1:
@@ -64,6 +69,20 @@ def draw_board(align_right: bool = False) -> str:
 
 # ИСПОЛЬЗОВАТЬ: все тесты отдельных модулей убираются под проверку импорта
 if __name__ == '__main__':
-    print(write_ini())
-    print(load_ini())
+    # print(load_ini())
+    # print(write_ini())
+    DIM = 5
+    RANGE = range(DIM)
+    BOARD = [str(i) for i in range(1, 26)]
     print(draw_board(False))
+
+
+#     1 | 2 | 3 | 4 | 5
+#  ————————————————————
+#    6 | 7 | 8 | 9 | 10
+#  ————————————————————
+# 11 | 12 | 13 | 14 | 15
+#  ————————————————————
+# 16 | 17 | 18 | 19 | 20
+#  ————————————————————
+# 21 | 22 | 23 | 24 | 25
